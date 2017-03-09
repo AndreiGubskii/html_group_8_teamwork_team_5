afd504d6f11bbcb199d1e9b23bbe217c3d7a0593
Isa <thebaurak@gmail.com>
2017-03-10 04:24:50 +0600

    add quick contact-info

---
+++ tech/buttons/js/main.js
@@ -0,0 +1,10 @@
+$(".contact-info").click(function(){   
+  $(this).next(".contact-block").show();
+});
+
+$(".contact-info").dblclick(function(){   
+  $(this).next(".contact-block").hide();
+});
+
+
+
